819bbaa6e634b1047761f498af8b39a9c8e700ee
chakravarthy <m.chakra473@gmail.com>
2022-03-13 18:47:19 +0530

    added remainder and quotient logic to uc6

--- Program.cs
+++ Program.cs
@@ -7,14 +7,14 @@ namespace basic_core_problems
         static void Main(string[] args)
         {
             Console.WriteLine("enter the dividend");
-            double dividend = int.Parse(Console.ReadLine());
+            int dividend = int.Parse(Console.ReadLine());
             Console.WriteLine("enter the divisor");
-            double divisor=int.Parse(Console.ReadLine());
-            double r;double q;
+            int divisor=int.Parse(Console.ReadLine());
+            int r, q;
             r= dividend%divisor;
             q= dividend/divisor;
             Console.WriteLine("Quotient is {0}", q);
-            Console.WriteLine("Remainder is {0}", r);
+            Console.WriteLine("Remainder is {0}",r);
            
         }
     }
