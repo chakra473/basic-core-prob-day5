32f3bb12b46627a97edd97eccf861eb51b758d26
chakravarthy <m.chakra473@gmail.com>
2022-03-13 19:19:22 +0530

    added largest among three numbers logic to uc10

--- Program.cs
+++ Program.cs
@@ -6,17 +6,23 @@ namespace basic_core_problems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("enter an alphabet to check whether it is vowel or not");
-            char vowel=char.Parse(Console.ReadLine());
-            switch (vowel)
+            Console.WriteLine("enter three numbers");
+            int a=int.Parse(Console.ReadLine());
+            int b=int.Parse(Console.ReadLine());
+            int c=int.Parse(Console.ReadLine());
+            if(a>b&&a>c)
             {
-                case 'a':Console.WriteLine("a is vowel");break;
-                    case 'e':Console.WriteLine("e is vowel");break;
-                case 'i':Console.WriteLine("i is vowel");break;
-                case 'o':Console.WriteLine("o is vowel");break;
-                case 'u':Console.WriteLine("u is vowel");break;
-                    default:Console.WriteLine("{0} is consonant",vowel);break;
+                Console.WriteLine("{0} is greatest among three numbers ",a);
             }
+            else if(b>c)
+            {
+                Console.WriteLine("{0} is greatest among three numbers",b);
+            }
+            else
+            {
+                Console.WriteLine("{0} is greatest among three numbers", c);
+            }
+            
 
            
             
