273133bbf940778c5cf7cbfcdd726b934e082fe6
chakravarthy <m.chakra473@gmail.com>
2022-03-13 18:00:19 +0530

    added coin flip logic to uc1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,33 @@ namespace basic_core_problems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("welcome to basic core problems");
+            int head = 0;
+            int tail = 0;
+            Random rand = new Random();
+
+            int i = 1;
+            while (i <= 100)
+            {
+                int random = rand.Next(0, 2);
+                if (random == 0)
+                {
+                    head++;
+
+                }
+                else
+                {
+                    tail++;
+
+                }
+                i++;
+
+
+
+            }
+            Console.WriteLine("number of heads :" + head);
+            Console.WriteLine("number of tails :" + tail);
+            Console.WriteLine("percentage of heads is {0}%", head);
+            Console.WriteLine("percentage of tails is {0}%", tail);
         }
     }
 }
