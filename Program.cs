d3862d0c94db008bdff347693461ad04d5d61509
chakravarthy <m.chakra473@gmail.com>
2022-03-13 17:52:34 +0530

    initialized

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace basic_core_problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to basic core problems");
+        }
+    }
+}
